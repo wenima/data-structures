d899772b29bad70f4509e448955366dc0ff76083
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-12 13:53:19 -0800

    folder structure folderchange

--- test_linked_list.py
+++ test_linked_list.py
@@ -12,6 +12,6 @@ def test_node_init():
     from linked_list import Node
     assert type(Node) == type
 
-@pytest.mark.parametrize(value, nxt, PARAMS_NODE_VALUES)
-def test_node_value(value, nxt):
+# @pytest.mark.parametrize(value, nxt, PARAMS_NODE_VALUES)
+# def test_node_value(value, nxt):
     """Test the contents of nodes created with given input."""
