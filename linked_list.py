d61eb67e1338640bae4e606041318812309aecca
Ted Callahan <CCallahanIV@gmail.com>
2016-12-12 13:49:28 -0800

    Wrote test for Node class, wrote Node class initialization, created setup.py

---
+++ linked_list.py
@@ -0,0 +1,8 @@
+"""Creating a Linked List Data Structure.  CF 401 Python Week 2 assignment."""
+
+
+class Node(object):
+    """Create Node objects for use in a Linked List data structure."""
+    def __init__(self, value, nxt=None):
+        self.value = value
+        self.nxt = None
---
+++ test_linked_list.py
@@ -0,0 +1,17 @@
+"""Testing the linked_list class - CF 401 Python Week 2 Assignment."""
+import pytest
+
+PARAMS_NODE_VALUES = [
+    ("something"),
+    ("something 2", "something 3")
+]
+
+
+def test_node_init():
+    """Test the initialization of a node."""
+    from linked_list import Node
+    assert type(Node) == type
+
+@pytest.mark.parametrize(value, nxt, PARAMS_NODE_VALUES)
+def test_node_value(value, nxt):
+    """Test the contents of nodes created with given input."""
