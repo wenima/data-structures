b30e149edf48b6012f437b1c5e4ed91936f3160b
Ted Callahan <CCallahanIV@gmail.com>
2016-12-13 13:56:13 -0800

    Successfully implemented Stack class composed of a Linked List.  Performed command line testing - intializing with list, popping from list until list is empty and raising an error, pushing values to list and popping them off again.

--- src/stack.py
+++ src/stack.py
@@ -2,11 +2,12 @@
 
 from linked_list import Linked_List
 
+
 class Stack(object):
     """The stack data structure is a composition of the Linked List structure."""
-    def __init__(self):
+    def __init__(self, iterable=None):
         """Initialize stack as a Linked_List-esque object."""
-        self._container = Linked_List()
+        self._container = Linked_List(iterable)
 
     def push(self, val):
         """Use Linked List push method to add one Node to stack."""
@@ -14,4 +15,4 @@ class Stack(object):
 
     def pop(self):
         """Use Linked List pop() method to remove one from stack."""
-        self._container.pop()
+        return self._container.pop()
