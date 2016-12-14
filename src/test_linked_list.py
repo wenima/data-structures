c54e77c52f8be33a8e29f6e6ea00426e8548b2e5
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-13 17:45:40 -0800

    100% testing success, put fixings into all the testfuntions where applicable and changed to point to fixtures

--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -3,11 +3,6 @@ import pytest
 
 PARAMS_SAMPLE_LIST = ["something", 1, "pear", 3, "apple"]
 
-@pytest.fixture
-def init_node():
-    from linked_list import Node
-    node = Node(0)
-    return str(type(node))
 
 @pytest.fixture
 def create_empty_node():
@@ -15,17 +10,18 @@ def create_empty_node():
     return Node()
 
 @pytest.fixture
-def create_ll_with_list():
+def new_ll():
     """Given a list, create a new Linked List with all the values."""
     from linked_list import Linked_List
-    new_ll = Linked_List(PARAMS_SAMPLE_LIST)
-    return new_ll
+    this_ll = Linked_List(PARAMS_SAMPLE_LIST)
+    return this_ll
 
 @pytest.fixture
-def create_empty_ll():
+def new_empty_ll():
+    """Create an empty object of type Stack to be used in test functions."""
     from linked_list import Linked_List
-    new_ll = Linked_List()
-    return new_ll
+    this_empty_ll = Linked_List()
+    return this_empty_ll
 
 
 def test_node_init():
@@ -36,6 +32,7 @@ def test_node_init():
 
 def test_node_instantiation(create_empty_node):
     """Test if the Node objects gets initiated correctly"""
+    from linked_list import Node
     assert create_empty_node.value is None
 
 
@@ -52,76 +49,73 @@ def test_linked_list_init():
     assert type(Linked_List) == type
 
 
-def test_linked_list_push():
+def test_linked_list_push(new_empty_ll):
     """Test the push method of the Linked List class."""
     from linked_list import Linked_List
-    new_ll = Linked_List()
-    new_ll.push("Something")
-    assert new_ll.head.value == "Something" and new_ll._size == 1
+    new_empty_ll.push("Something")
+    assert new_empty_ll.head.value == "Something" and new_empty_ll._size == 1
 
 
-def test_linked_list_search_success():
+def test_linked_list_search_success(new_ll):
     """Test the search method of the Linked List class with a search query that exists."""
     from linked_list import Linked_List
-    new_linked_list = Linked_List(PARAMS_SAMPLE_LIST)
-    result = new_linked_list.search('pear')
+    result = new_ll.search('pear')
     assert result.value == 'pear'
 
 
-def test_linked_list_search_failure():
+def test_linked_list_search_failure(new_ll):
     """Test the search method of the Linked List class with a search query that DOES NOT exist."""
     from linked_list import Linked_List
-    new_linked_list = Linked_List(PARAMS_SAMPLE_LIST)
-    result = new_linked_list.search('owt')
+    result = new_ll.search('owt')
     assert result.startswith('That')
 
 
-def test_linked_list_pop():
+def test_linked_list_pop(new_ll):
     """Test the pop method of the Linked List class."""
     from linked_list import Linked_List
-    new_ll = Linked_List(PARAMS_SAMPLE_LIST)
     old_head_val = new_ll.head.value
     new_ll.pop()
     new_head_val = new_ll.head.value
     assert old_head_val == "apple" and new_head_val == 3
 
+def test_pop_from_empty_list(new_empty_ll):
+    """Tests behaviour of Linked_List.pop function when executed on empty
+    linked list"""
+    from linked_list import Linked_List
+    with pytest.raises(IndexError):
+        new_empty_ll.pop()
 
-def test_linked_list_display():
+
+def test_linked_list_display(new_ll):
     """Test the display method of the Linked_List class."""
     from linked_list import Linked_List
-    new_linked_list = Linked_List(["something", "something else", "two"])
-    result = "('two', 'something else', 'something')"
-    assert new_linked_list.display() == result
+    result = str(("apple", 3, "pear", 1, "something"))
+    assert new_ll.display() == result
 
 
-def test_linked_list_size():
+def test_linked_list_size(new_ll):
     """Test if the size method of the Linked_List class returns the size
     correctly by independently iterating over the the linked list"""
     from linked_list import Linked_List
-    test_l = len(PARAMS_SAMPLE_LIST)
-    new_linked_list = Linked_List(PARAMS_SAMPLE_LIST)
-    assert new_linked_list._size == test_l
-    assert len(new_linked_list) == test_l
-    assert new_linked_list.size() == test_l
+    assert new_ll._size == len(PARAMS_SAMPLE_LIST)
+    assert len(new_ll) == len(PARAMS_SAMPLE_LIST)
+    assert new_ll.size() == len(PARAMS_SAMPLE_LIST)
 
 
-def test_linked_list_remove_node_exists():
+def test_linked_list_remove_node_exists(new_ll):
     """Test if the remove method of the Linked_List class is correctly
     removing a node"""
     from linked_list import Linked_List
-    ll = Linked_List(PARAMS_SAMPLE_LIST)
-    r_node = ll.search("pear")
-    ll.remove(r_node)
-    assert ll.size() == 4
+    r_node = new_ll.search("pear")
+    new_ll.remove(r_node)
+    assert new_ll.size() == 4
     #result.startswith("Success")
 
 
-def test_linked_list_remove_node_not_exists():
+def test_linked_list_remove_node_not_exists(new_ll):
     """Test if the remove method of the Linked_List class is correctly behaving
     if the node does not exist"""
-    from linked_list import Linked_List
-    ll = Linked_List(PARAMS_SAMPLE_LIST)
+    from linked_list import Linked_List, Node
+    r_node = Node("bear")
     with pytest.raises(ValueError):
-        r_node = Node("bear")
-        ll.remove(r_node)
-    
+        new_ll.remove(r_node)
