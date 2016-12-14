7f4eafea7d7bd7b6e256a091c42a4227550ddc20
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-13 20:33:28 -0800

    updated README to hold more information about the program; updates setup.py based on instructor's comments

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name="linked_list",
-    description="Week 2 Day 1 Assignment - Linked List",
-    version=0.0,
+    name="data-structures",
+    description="this module will be expanded as we write more data-structures",
+    version=1.1,
     author="Ted Callahan and Mark Kessler-Wenicker",
     author_email="",
     license="MIT",
@@ -11,9 +11,5 @@ setup(
     py_modules=["linkedlist"],
     install_requires=[],
     extras_require={"test": ["pytest", "pytest-watch", "pytest-cov", "tox"]},
-    entry_points={
-        'console_scripts': [
-            "linked_list = linked_list:main"
-        ]
-    }
+    entry_points={}
 )
