d6dc797489e44ff56a985fdb82eb164f5171cfcf
Ted Callahan <CCallahanIV@gmail.com>
2016-12-15 14:20:13 -0800

    Added queue to py_modules in setup.py.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email="",
     license="MIT",
     package_dir={'': 'src'},
-    py_modules=["linkedlist", "stack", "dbl_linked_list"],
+    py_modules=["linkedlist", "stack", "dbl_linked_list", "queue"],
     install_requires=[],
     extras_require={"test": ["pytest", "pytest-watch", "pytest-cov", "tox"]},
     entry_points={}
