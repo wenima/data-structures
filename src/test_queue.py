bd933dcd9aee15c2243e77e5da2fc0f6285b13e2
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-18 12:26:11 -0800

    added tests https://github.com/wenima/data-structures/issues/14

--- src/test_queue.py
+++ src/test_queue.py
@@ -34,6 +34,12 @@ def new_empty_q():
     this_empty_q = Queue()
     return this_empty_q
 
+@pytest.fixture
+def new_q_1():
+    from queue import Queue
+    this_q = Queue(TEST_ITER[-1])
+    return this_q
+
 
 def test_create_empty_dbl_node(create_empty_node):
     """Test creation of an empty node."""
@@ -42,7 +48,7 @@ def test_create_empty_dbl_node(create_empty_node):
     assert create_empty_node.prev is None
 
 
-def test_create_node_with_val_nxt_prev(): #multiple asserts are bad
+def test_create_node_with_val_nxt_prev():  #is that doing anyting past assert?
     """Test creation of DblNodes with values and nxt, prev references."""
     from dbl_linked_list import DblNode
     node1 = DblNode(1)
@@ -52,28 +58,21 @@ def test_create_node_with_val_nxt_prev(): #multiple asserts are bad
     node3 = DblNode([3], node1, node2)
     assert node3.value == [3] and node3.nxt is node1 and node3.prev is node2
 
+
 def test_create_empty_Queue(new_empty_q):
     """Test creation of empty Queue."""
     assert new_empty_q._container.head is None
     assert new_empty_q._container.tail is None
 
 
-def test_create_queue_with_one_value():
+def test_create_queue_with_one_value(new_q_1):
     """Test that a queue can be created with one value."""
-    from queue import Queue
-    new_q = Queue(TEST_ITER[-1])
-    assert new_q._container.head.value == TEST_ITER[-1] and new_q._container.tail.value == TEST_ITER[-1]
-    assert new_q._container.head.nxt is None
-    assert new_q._container.head.prev is None
-    assert new_q._container.tail.nxt is None
-    assert new_q._container.tail.prev is None
+    assert new_q_1._container.head.value == TEST_ITER[-1] and new_q_1._container.tail.value == TEST_ITER[-1]
+    assert new_q_1._container.head.nxt is None
+    assert new_q_1._container.head.prev is None
+    assert new_q_1._container.tail.nxt is None
+    assert new_q_1._container.tail.prev is None
 
-# def test_create_queue_with_one_value_assign_head_tail(new_q): #refactor this / rolled into test_create_queue_with_one_value
-#     """Test that a list created with one value has correct prev/next."""
-#     assert new_q._container.head.nxt is None
-#     assert new_q._container.head.prev is None
-#     assert new_q._container.tail.nxt is None
-#     assert new_q._container.tail.prev is None
 
 def test_create_queue_with_iterable(new_q):
     """Given an iterable, test that new Queue is created correctly."""
@@ -92,26 +91,33 @@ def test_create_queue_with_iter_correct_prev_nxt(new_q):
     assert new_q._container.head.nxt.value == TEST_ITER[1]
     assert new_q._container.tail.prev.value == TEST_ITER[-2]
 
-#Enqueue SPECIFIC TESTS
+
+"""Enqueue SPECIFIC TESTS"""
+
+
 def test_enque_empty_list(new_empty_q):
     """Test enqueued val is new tail."""
     new_empty_q.enqueue(TEST_ITER[-1])
     assert new_empty_q._container.head.value == TEST_ITER[-1] and new_empty_q._container.tail.value == TEST_ITER[-1]
+
 
 def test_enqueue_empty_queue_new_head_tail(new_empty_q):
     """Test that enquing on an empty queue results in a new head, tail."""
     new_empty_q.enqueue(TEST_ITER[-1])
     assert new_empty_q._container.head is new_empty_q._container.tail
 
+
 def test_first_enqueued_val_nxt_prev_is_none(new_empty_q):
     """Test that the first enqueued DblNode has no nxt or prev."""
     new_empty_q.enqueue(TEST_ITER[-1])
     assert new_empty_q._container.head.nxt is None and new_empty_q._container.head.prev is None
 
+
 def test_enqueued_assigns_new_head(new_q):
     """Test new tail is assigned."""
     new_q.enqueue(6)
     assert new_q._container.tail.value == 6
+
 
 def test_enqueue_reassigns_prev_nxt(new_q):
     """Test that enqueing to a full list correctly assigns prev, nxt."""
@@ -120,17 +126,22 @@ def test_enqueue_reassigns_prev_nxt(new_q):
     assert new_q._container.tail.prev.value == old_tail.value
     assert new_q._container.tail.prev.nxt is new_q._container.tail
 
+
 def test_enqueue_increases_size(new_q):
     """Test that enqueing a node is increasing the size by 1"""
     old_size = new_q._container._size
     new_q.enqueue(6)
     assert new_q._container._size == old_size + 1
 
-# """DEQUEUE SPECIFIC TESTS"""
+
+"""DEQUEUE SPECIFIC TESTS"""
+
+
 def test_dequeue_empty_list_raise_error(new_empty_q):
     """Test that dequeing an empty list raises an Index Error."""
     with pytest.raises(IndexError):
         new_empty_q.dequeue()
+
 
 def test_dequeue_reassign_head(new_q):
     """Test that dequeueing a non-empty list reassigns head."""
@@ -138,11 +149,13 @@ def test_dequeue_reassign_head(new_q):
     new_q.dequeue()
     assert old_head.nxt.value == new_q._container.head.value
 
+
 def test_dequeue_decrease_size(new_q):
     """Test that dequeue correctly decreases size."""
     old_size = new_q._container._size
     new_q.dequeue()
     assert new_q._container._size == old_size - 1
+
 
 def test_dequeue_reassign_nxt_prev(new_q):
     """Test that dequeueing a non-empty list reassigns head.nxt and head.nxt.prev."""
@@ -155,11 +168,16 @@ def test_dequeue_returns_correct_value(new_q):
     head_value = new_q._container.head.value
     assert new_q.dequeue() == head_value
 
-#Peek specific TESTS
+
+"""Peek specific TESTS """
+
+
 def test_peek_returns_None_on_empty_queue(new_empty_q):
     """Test that peeking on an empty Queue object returns None"""
     assert new_empty_q.peek() is None
 
+
 def test_peek_returns_first_in_queue(new_q):
     """Test that peek returns the value of the first node in the Queue object"""
-    assert new_q.peek() == new_q._container.head.value
+    head_before_peek = new_q._container.head
+    assert new_q.peek() == new_q._container.head.value and new_q._container.head == head_before_peek
