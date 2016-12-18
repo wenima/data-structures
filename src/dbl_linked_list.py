c7b7d07d5444a046b29b10fac1ea54e392ca66c5
Ted Callahan <CCallahanIV@gmail.com>
2016-12-17 16:41:21 -0800

    Resolved issues 28, 30, 33, 36, 37. Fixed docstrings and formatting, fixed line breaks between methods, refactored push and append.  added __len__() method.

--- src/dbl_linked_list.py
+++ src/dbl_linked_list.py
@@ -5,10 +5,11 @@ class Dbl_Node(object):
     """Create Node objects for use in a Linked List data structure.
 
     Attributes:
-    value:      A value stored in the Node.
-    nxt:        Pointer to the next Node, used in a Linked List or Stack
-                data structure.
+        value:      A value or data stored in the Node.
+        nxt:        Pointer to the next Node.
+        prev:       A pointer to a previus Node.
     """
+
     def __init__(self, value=None, nxt=None, prev=None):
         """Initialize a Node type object."""
         self.value = value
@@ -17,16 +18,21 @@ class Dbl_Node(object):
 
 
 class Dbl_Linked_List(object):
-    """Double Linked List style Data Structure
+    """Double Linked List (DLL) style Data Structure.
 
-        Attributes:
-        head:       Always the most recent item added to a Linked List.
-        _size:      The length of the Linked List or number of Nodes stored
+    If initialized with an iterable, will create nodes for each item in
+    the iterable.
+
+    Attributes:
+        head:       Node on one end of DLL, last item added if
+                    initialized with iterable.
+        tail:       Node on other end of DLL, first item added if
+                    intailized with iterable.
+        _size:      The length of the DLL or number of Nodes stored
                     in the list.
 
-        Methods:
-
-        push(value):    Given a value, add a new Node object to a Linked List.
+    Methods:
+        push(value):    Given a value, add a new Node object to the DLL.
 
         append(value):  Given a value, adds it at the end of the line.
 
@@ -34,12 +40,12 @@ class Dbl_Linked_List(object):
 
         shift():        will remove the last value from the line and return it.
 
-        remove(val):    Given a Node, remove that Node from the Linked List.  Else,
-                        raise a ValueError.
-        """
+        remove(val):    Given a Node, remove that Node from the Linked List.
+                        Else, raise a ValueError.
+    """
 
     def __init__(self, maybe_an_iterable=None):
-        """Intialize a doubly linked List Object."""
+        """Intialize a Double Linked List Object."""
         self.head = None
         self.tail = None
         self._size = 0
@@ -53,22 +59,22 @@ class Dbl_Linked_List(object):
                 self.push(maybe_an_iterable)
 
     def push(self, value):
-        """Add a node at the beginning of the line."""
-        self.head = Dbl_Node(value, self.head)
-        self._size += 1
-        if self._size == 1:
-            self.tail = self.head
-        elif self._size == 2:
-            nodes = [node for node in self._iterate_from(self.head)]
-            self.tail = nodes[-1]
-            nodes[-1].prev = self.head
+        """Add a node at the beginning of list and reassign head."""
+        new = Dbl_Node(value)
+        if self._size > 0:
+            old = self.head
+            self.head = new
+            new.nxt = old
+            old.prev = new
         else:
-            self.head.nxt.prev = self.head
+            self.head = new
+            self.tail = self.head
+        self._size += 1
 
     def pop(self):
-        """Pop the first value off the head of LL and return it."""
+        """Remove head node from DLL and return its value."""
         if self.head is None:
-            raise IndexError("Cannot pop from an empty Linked List.")
+            raise IndexError("Cannot pop from an empty Double Linked List.")
         val = self.head.value
         self.head = self.head.nxt
         self.head.prev = None
@@ -76,9 +82,9 @@ class Dbl_Linked_List(object):
         return val
 
     def shift(self):
-        """will remove the last value from the tail of the list and return it."""
+        """Remove tail node from DLL and return its value."""
         if self.tail is None:
-            raise IndexError("Cannot shift (remove the last node in the line) from an empty Linked List.")
+            raise IndexError("Cannot shift from an empty Double Linked List.")
         val = self.tail.value
         self.tail.prev.nxt = None
         self.tail = self.tail.prev
@@ -86,21 +92,20 @@ class Dbl_Linked_List(object):
         return val
 
     def append(self, value):
-        """will append a new node at the end of the line and will set the new tail to be this node."""
-        new_node = Dbl_Node(value, None, self.tail)
-        nodes = [node for node in self._iterate_from(self.head)]
-        if nodes:
-            self.tail = new_node
-            nodes[-1].nxt = self.tail
-            self._size += 1
+        """Append a new node at the end of list and reassign tail."""
+        new = Dbl_Node(value)
+        if self._size > 0:
+            old = self.tail
+            self.tail = new
+            new.prev = old
+            old.nxt = new
         else:
-            self.head = new_node
-            self.tail = new_node
-            self._size += 1
+            self.tail = new
+            self.head = self.tail
+        self._size += 1
 
     def remove(self, value):
-        """Remove the given node from the LL."""
-        nodes = [node for node in self._iterate_from(self.head)]
+        """Remove the given node from the DLL."""
         r_node = [node for node in self._iterate_from(self.head) if node.value == value]
         if not r_node:
             raise ValueError("ERROR: That node is not in this linked list.")
@@ -119,7 +124,11 @@ class Dbl_Linked_List(object):
                     return "Succesfully removed Node with value: {}".format(r_node[0].value)
 
     def _iterate_from(self, list_item):
-        """return a generator"""
+        """Return a generator."""
         while list_item is not None:
             yield list_item
             list_item = list_item.nxt
+
+    def __len__(self):
+        """Return _size of DLL."""
+        return self._size
