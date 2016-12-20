635dfbbf2c03965bfd286290a9edd481f0385265
Marc Fieser <midfies@gmail.com>
2016-12-20 12:50:47 -0800

    built basic class structure

---
+++ src/binheap.py
@@ -0,0 +1,35 @@
+"""Implementation of the binheap data structure."""
+
+
+class Binheap(object):
+    """Create a binary heap tree structure."""
+
+    def __init__(self):
+        """Initialize an instance of the binheap."""
+        self._heap = [0]
+        self._size = 0
+
+    def push(self, value):
+        """Push a new value to the heap."""
+        pass
+
+    def pop(self):
+        """Remove the root node from the tree."""
+        pass
+
+    def insert(self, value):
+        """Insert value into the tree structure and reorganize."""
+        pass
+
+    def raise_up(self, i):
+        """Raise i into the tree until the tree structure is satisfied."""
+        pass
+
+    def sink_down(self, i):
+        """Sink i down the tree until the tree structure is satisfied."""
+        pass
+
+    def get_min_child(self, i):
+        """Determine the smaller of two children."""
+        pass
+        
