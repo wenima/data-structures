74a6c46373d49fa62eef07bcb2df7bf77f2473a5
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-20 09:07:02 -0800

    removed comments in the docstring referring to the method of previous meaning

--- src/deque.py
+++ src/deque.py
@@ -18,13 +18,13 @@ class Deque(object):
 
             append(val):  Add a new node to the end (tail) of the queue.
 
-            appendleft(val) (was push()): adds a value to the front of the deque
+            appendleft(val): adds a value to the front of the deque
 
-            pop() (was shift): removes a value from the end of the deque and returns it (raises an exception if the deque is empty)
+            pop(): removes a value from the end of the deque and returns it (raises an exception if the deque is empty)
 
-            popleft (was pop): removes a value from the front of the deque and returns it (raises an exception if the deque is empty)
+            popleft: removes a value from the front of the deque and returns it (raises an exception if the deque is empty)
 
-            peek() (aka reverse peek): returns the next value that would be returned by pop but leaves the value in the deque (returns None if the deque is empty)
+            peek(): returns the next value that would be returned by pop but leaves the value in the deque (returns None if the deque is empty)
 
             peekleft(): returns the next value that would be returned by popleft but leaves the value in the deque (returns None if the deque is empty)
 
