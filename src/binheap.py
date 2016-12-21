7e3a248a702c32f80a1552702d8eeb9c4800e4cf
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-20 17:17:46 -0800

    added sink_down function

--- src/binheap.py
+++ src/binheap.py
@@ -21,11 +21,17 @@ class Binheap(object):
         """Push a new value to the heap."""
         self._heap.append(value)
         self._raise_up(len(self._heap) - 1)
-        return self._heap
+        self._size += 1
 
     def pop(self):
         """Remove the root node from the tree."""
-        self._heap
+        if self._size == 0:
+            raise IndexError("You can't remove the head of an empty binary heap")
+        root = self._heap[1]
+        self._heap[1] = self._heap[-1]
+        del self._heap[-1]
+        # self._sink_down()
+        return root
 
     def _insert(self, value):
         """Insert value into the tree structure and reorganize."""
@@ -33,7 +39,6 @@ class Binheap(object):
 
     def _raise_up(self, i):
         """Raise i into the tree until the tree structure is satisfied."""
-        print(self._heap, i)
         while i // 2 > 0:
             if self._heap[i] < self._heap[i // 2]:
                 tmp = self._heap[i // 2]
@@ -42,10 +47,25 @@ class Binheap(object):
             i = i // 2
         return self._heap
 
+
     def _sink_down(self, i):
-        """Sink i down the tree until the tree structure is satisfied."""
-        pass
+        """Sink the input node down the tree until the tree structure is satisfied."""
+        parent = self._heap[i]
+        while i * 2 < len(self._heap):
+            child = self._heap[self._get_min_child(i)]
+            idx_child = self._get_min_child(i)
+            if parent > child:
+                self._heap[i], self._heap[idx_child] = self._heap[idx_child], self._heap[i]
+            parent = self._heap[idx_child]
+            i = idx_child
+        return self._heap
+
 
     def _get_min_child(self, i):
         """Determine the smaller of two children."""
-        pass
+        if i * 2 == self._size:
+            return i * 2
+        if self._heap[i * 2] > self._heap[i * 2 + 1]:
+            return i * 2 + 1
+        else:
+            return i * 2
