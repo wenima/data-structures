aac950ec5a8f5a6c56c8626e469087b9c16d6621
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-21 17:22:26 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="data-structures",
-    description="this module will be expanded as we write more data-structures",
+    name="Priority Queue",
+    description="In a priority queue, an element with a high priority will be served before an element with a lower priority. If both have equal priority, the first element that entered the list will be added",
     version=1.1,
     author="Marc Fieser and Mark Kessler-Wenicker",
     author_email="",
     license="MIT",
     package_dir={'': 'src'},
-    py_modules=["linkedlist", "stack", "dbl_linked_list"],
+    py_modules=["binheap, priorityq"],
     install_requires=[],
     extras_require={"test": ["pytest", "pytest-watch", "pytest-cov", "tox"]},
     entry_points={}
