d98eee80eca8fc5c9e74c26453abf767a37a24c2
Marc Fieser <midfies@gmail.com>
2016-12-23 12:53:58 -0800

    minor bug fixes

--- src/simple_graph.py
+++ src/simple_graph.py
@@ -14,29 +14,39 @@ class Graph(object):
         edges = []
         for node in self._nodes:
             for edge in self._nodes[node]:
-                edge = str(node + '-' + edge)
+                edge = str(node) + '-' + str(edge)
                 edges.append(edge)
         return edges 
 
 
     def add_node(self, n):
-        self._nodes['n'] = []
+        if not self.has_node(n):
+            self._nodes[n] = []
 
     def add_edge(self, n1, n2):
-        self._nodes['n1'].append(n2)
+        if not self.has_node(n1):
+            self.add_node(n1)
+        if not self.has_node(n2):
+            self.add_node(n2)
+        self._nodes[n1].append(n2)
 
-    def del_node(self, n)
-        del self._nodes[n]
-        for node in self._nodes:
-            for edge in self._nodes[node]:
-                if edge == n:
-                    self._nodes[node].remove(n)
+    def del_node(self, n):
+        if self.has_node(n):
+            del self._nodes[n]
+            for node in self._nodes:
+                for edge in self._nodes[node]:
+                    if edge == n:
+                        self._nodes[node].remove(n)
+        else:
+            raise(KeyError)
 
-    def del_edge(self, n1, n2)
-        self[n1].remove(n2)
+    def del_edge(self, n1, n2):
+        if n2 in self._nodes[n1]:
+            self._nodes[n1].remove(n2)
+        raise (ValueError)
 
     def has_node(self, n):
-        if self._node[n]:
+        if n in self._nodes:
             return True
         return False
 
@@ -44,6 +54,8 @@ class Graph(object):
         return self._nodes[n]
 
     def adjacent(self, n1, n2):
+        if not self.has_node(n1) or not self.has_node(n2):
+            raise(KeyError)
         if n2 in self._nodes[n1]:
             return True
         return False
