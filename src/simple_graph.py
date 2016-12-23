17117a813c2658078ab255cd264d467baaadbd1c
Marc Fieser <midfies@gmail.com>
2016-12-23 12:13:42 -0800

    built out Graph class

--- src/simple_graph.py
+++ src/simple_graph.py
@@ -4,32 +4,46 @@ class Graph(object):
     """A graph class."""
 
     def __init__(self):
-        self._graph = {}
+        self._nodes = {}
 
 
     def nodes(self):
-        return self._graph.keys()
+        return self._nodes.keys()
 
     def edges(self):
-        pass
+        edges = []
+        for node in self._nodes:
+            for edge in self._nodes[node]:
+                edge = str(node + '-' + edge)
+                edges.append(edge)
+        return edges 
+
 
     def add_node(self, n):
-        pass
+        self._nodes['n'] = []
 
     def add_edge(self, n1, n2):
-        pass
+        self._nodes['n1'].append(n2)
 
     def del_node(self, n)
-        pass
+        del self._nodes[n]
+        for node in self._nodes:
+            for edge in self._nodes[node]:
+                if edge == n:
+                    self._nodes[node].remove(n)
 
     def del_edge(self, n1, n2)
-        pass
+        self[n1].remove(n2)
 
     def has_node(self, n):
-        pass
+        if self._node[n]:
+            return True
+        return False
 
     def neighbors(self, n):
-        pass
+        return self._nodes[n]
 
     def adjacent(self, n1, n2):
-        pass
+        if n2 in self._nodes[n1]:
+            return True
+        return False
