e7a9c70aa3a40e9b668b4965757ebefb257e7402
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-23 13:01:29 -0800

    typos

--- src/test_simple_graph.py
+++ src/test_simple_graph.py
@@ -43,11 +43,11 @@ def test_adding_edge_to_graph_w_1_node(g1):
     """Test that adding an edge to empty graph adds new node where one of the 2
     doesn't exist."""
     new_nodes = ['A', 'B']
-    g_empty.add_edge('A', 'B')
+    g1.add_edge('A', 'B')
     for n in new_nodes:
         assert n in nodes.keys()
-    assert len(g_empty) == 2
-    assert g_empty.['A'] == ['B']
+    assert len(g1) == 2
+    assert g1._nodes['A'] == ['B']
 
 def test_delete_node_raises_error_if_node_doesn_exist(g1):
     """Test that deleting a node which doesn't exist raises an error message of
@@ -60,7 +60,7 @@ def test_delete_node_removed_from_graph(g1):
     g1.add_edge('A', 'C')
     g1.del_node('A')
     assert len(g1) == 1
-    assert g1.nodes['C'] is False
+    assert g1._nodes['C'] is False
 
 def test_delete_edge_raises_error_if_not_exist(g1):
     """Test that deleting an edge raises a ValueError if the edge doesn't exist
