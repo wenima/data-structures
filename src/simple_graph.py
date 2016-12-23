5de34cc03b3579fcea0bd5ccad03cbc8a288860d
Marc Fieser <midfies@gmail.com>
2016-12-23 15:29:04 -0800

    docstrings

--- src/simple_graph.py
+++ src/simple_graph.py
@@ -1,29 +1,53 @@
 """An implementation of a simple graph."""
 
+
 class Graph(object):
-    """A graph class."""
+    """A graph class.
+
+    nodes() - Return list of nodes in graph
+
+    edges() - Return all edges in graph
+
+    add_node(n) -add node n to graph
+
+    add_edge(n1. n2) - add edge between n1 and n2 add nodes if not already in graph
+
+    del_node(n) - delete node n
+
+    del_edge(n1, n2) - del edge between n1 and n2
+
+    has_node(n) - check if node n is in graph
+
+    neighbors(n) - return list of neighbors of n
+
+    adjacent(n1, n2) - check if n1 and n2 are adjacent.
+
+    """
 
     def __init__(self):
+        """Initialize a graph with a dict."""
         self._nodes = {}
 
-
     def nodes(self):
+        """Return list of nodes."""
         return self._nodes.keys()
 
     def edges(self):
+        """Return list of edges."""
         edges = []
         for node in self._nodes:
             for edge in self._nodes[node]:
                 edge = str(node) + '-' + str(edge)
                 edges.append(edge)
-        return edges 
-
+        return edges
 
     def add_node(self, n):
+        """Add a node to the graph."""
         if not self.has_node(n):
             self._nodes[n] = []
 
     def add_edge(self, n1, n2):
+        """Add edge to graph, if nodes not in graph, add them."""
         if not self.has_node(n1):
             self.add_node(n1)
         if not self.has_node(n2):
@@ -31,6 +55,7 @@ class Graph(object):
         self._nodes[n1].append(n2)
 
     def del_node(self, n):
+        """Delete node from list and remove any reference to it in other nodes."""
         if self.has_node(n):
             del self._nodes[n]
             for node in self._nodes:
@@ -41,20 +66,24 @@ class Graph(object):
             raise(KeyError)
 
     def del_edge(self, n1, n2):
+        """Remove edge from list."""
         if n2 in self._nodes[n1]:
             self._nodes[n1].remove(n2)
         else:
             raise (ValueError)
 
     def has_node(self, n):
+        """.Check if node is in graph."""
         if n in self._nodes:
             return True
         return False
 
     def neighbors(self, n):
+        """Return list of neighbors nodes to node n."""
         return self._nodes[n]
 
     def adjacent(self, n1, n2):
+        """Return True if n1 and n2 are adjacent to each other."""
         if not self.has_node(n1) or not self.has_node(n2):
             raise(KeyError)
         if n2 in self._nodes[n1]:
