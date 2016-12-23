c8b3349f276d28874a3d779f7630e546ceed51ab
Marc Fieser <midfies@gmail.com>
2016-12-23 11:28:40 -0800

    added new files for graph

---
+++ src/simple_graph.py
@@ -0,0 +1,35 @@
+"""An implementation of a simple graph."""
+
+class Graph(object):
+    """A graph class."""
+
+    def __init__(self):
+        self._graph = {}
+
+
+    def nodes(self):
+        return self._graph.keys()
+
+    def edges(self):
+        pass
+
+    def add_node(self, n):
+        pass
+
+    def add_edge(self, n1, n2):
+        pass
+
+    def del_node(self, n)
+        pass
+
+    def del_edge(self, n1, n2)
+        pass
+
+    def has_node(self, n):
+        pass
+
+    def neighbors(self, n):
+        pass
+
+    def adjacent(self, n1, n2):
+        pass
