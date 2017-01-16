da26b0467f4bf279496a2b196ee838a3411df486
Marc Kessler <marc.kesslernyc@gmail.com>
2017-01-16 13:41:42 -0800

    updated setup.py to more meaningful description

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name="simple_graph",
-    description="this module demonstrates the graph data structure",
-    version=1.1,
-    author="Marc Fieser and Marc Kessler-Wenicker",
+    name="Codefellows Seattle Advanced Python 401 Course - Data Structure Assignments",
+    description="this module holds all the data structures we built as part of data structure assignments",
+    version=0.9,
+    author="Marc Fieser, Ford Fowler, Ted Callahan, Marc Kessler-Wenicker",
     author_email="",
     license="MIT",
     package_dir={'': 'src'},
