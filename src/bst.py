c666c7b3ebeed0f7cb60e1d409f44f303fab99a6
Ford Fowler <fordjfowler@gmail.com>
2017-01-16 13:49:53 -0800

    iterable init

--- src/bst.py
+++ src/bst.py
@@ -3,41 +3,57 @@
 
 class TreeNode(object):
     """TreeNode is the root of the tree."""
+
     def __init__(self, val, left=None, right=None, parent=None):
+        """."""
         self.val = val
         self.left = left
         self.right = right
         self.parent = parent
 
     def is_root(self):
+        """."""
         return not self.parent
 
     def is_leaf(self):
+        """."""
         return not (self.right or self.left)
 
     def has_both_children(self):
+        """."""
         return self.right and self.left
 
     def left_or_right(self, val):
+        """."""
         if val < self.val:
             return self.left
         return self.right
 
 
-class bst(object):
+class BST(object):
     """Binary search tree."""
 
-    def __init__(self):
+    def __init__(self, iterable=None):
+        """Initialize bst with root and size."""
         self.root = None
         self.size = 0
+        if iterable:
+            try:
+                for val in iterable:
+                    self.insert(val)
+            except TypeError:
+                self.insert(iterable)
 
     def length(self):
+        """Return number of nodes in bst.."""
         return self.size
 
     def __len__(self):
+        """Return number of nodes in bst."""
         return self.size
 
     def insert(self, val):
+        """Insert a new node into the bst."""
         cur = self.root
         if cur is None:
             self.root = TreeNode(val)
@@ -64,3 +80,7 @@ class bst(object):
         elif val == start.val:
             return start
         return self.search(val, start=start.left_or_right(val))
+
+    def contains(self, val):
+        """Return whether val in bst."""
+        return self.search(val) and True
