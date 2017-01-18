4501402bf6ed3f11f713e52c9fccccc1a57b9696
Marc Kessler <marc.kesslernyc@gmail.com>
2017-01-18 13:56:06 -0800

    tests added for deletion of node with 2 children.

--- src/test_bst_delete.py
+++ src/test_bst_delete.py
@@ -4,7 +4,7 @@ import pytest
 import types
 
 
-TEST_BST1 = [50, 30, 70, 20, 40, 80, 60, 65, 75, 85, 32, 34, 36, 10]
+TEST_BST1 = [50, 30, 70, 20, 40, 80, 60, 65, 75, 85, 32, 34, 36, 10, 76]
 
 
 TEST_BST_DELETE = [
@@ -17,9 +17,15 @@ TEST_BST_DELETE = [
     #delete a node with a tree under it on the right side
     (32, 40, [34]),
     # #delete a node with 2 children under it
-    # (70, 75, [60, 80]),
-    # #delete root
-    # (50, 60, [30, 70]).
+    (30, 50, [32, 70]),
+    (30, 32, [20, 40]),
+    (30, 40, [34]),
+    (70, 75, [60, 80]),
+    (70, 50, [30, 75]),
+    (70, 80, [76, 85]),
+    # delete root
+    (50, 60, [30, 70]),
+    (50, 70, [65, 80]),
 ]
 
 
@@ -57,3 +63,5 @@ def test_delete_returns_correct_children(d, n, result, bst1):
     bst1.delete(d)
     children = [c.val for c in bst1.search(n).children()]
     assert children == result
+
+    
