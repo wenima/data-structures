7f3c1b73fba616ac6699e63ae97881aa0eb31a15
Marc Kessler <marc.kesslernyc@gmail.com>
2017-01-19 15:09:09 -0800

    added right rotation

--- src/avl.py
+++ src/avl.py
@@ -83,3 +83,29 @@ class AVL(BST):
                 r_root.parent.right = n_root
         n_root.left = r_root
         r_root.parent = n_root
+
+    def rr(self, r_root):
+        """Perform a right rotation (rl) around the rotation_root (r_root)."""
+        #assume:
+        #           c
+        #          /
+        #         b
+        #        /
+        #       a
+        #1. b becomes new root
+        #2. c takes responsibility of b's right child as it's left child
+        #3. b takes responsibility of c as it's right child
+        n_root = r_root.left
+        r_root.left = n_root.right
+        if n_root.right != None:
+            n_root.right.parent = r_root
+            n_root.parent = r_root.parent
+        if r_root.is_root():
+            self.root = n_root
+        else:
+            if r_root.is_right():
+                r_root.parent.right = n_root
+            else:
+                r_root.parent.right = n_root
+        n_root.right = r_root
+        r_root.parent = n_root
