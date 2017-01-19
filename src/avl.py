42e9c4d28e6fa8d50d74f3e191a0bdffabed848f
Marc Kessler <marc.kesslernyc@gmail.com>
2017-01-19 14:54:19 -0800

    added rebalance to figure out which rotation is needed, added left rotation function

--- src/avl.py
+++ src/avl.py
@@ -34,27 +34,52 @@ class AVL(BST):
             else:
                 break
 
+    def rebalance(self,n):
+        """Rebalance a tree starting at node n. We assume that a tree heavy on the right side will
+        have a postive balance and trees heavy on the left side a negative value."""
+        #1. If subtree needs LR to balance:
+        #   check balance factor for the right child:
+        #   If the right child is left heavy, then do a RR on right child,
+        #   followed by LR.
+        #2. If a subtree needs a RR to balance:
+        #   check balance factor of the left child:
+        #   if left child is right heavy, do a LR on the left child,
+        #   followed by RR.
+        if b.balance(n) > 0:
+            if b.balance(n.right) < 0:
+                self.rr(n.right)
+                self.lr(n)
+            else:
+                self.lr(n)
+        elif b.balance(n) < 0:
+            if b.balance(n.left) > 0:
+                self.lr(n.left)
+                self.rr(n)
+            else:
+                self.rr(n)
 
-
-    # def rebalance(self,node):
-    #     """Rebalance a tree."""
-    #     #1. If subtree needs LR to balance:
-    #     #   check balance factor for the right child:
-    #     #   If the right child is left heavy, then do a RR on right child,
-    #     #   followed by LR.
-    #     #2. If a subtree needs a RR to balance:
-    #     #   check balance factor of the left child:
-    #     #   if left child is right heavy, do a LR on the left child,
-    #     #   followed by RR.
-    #     if n.bf < 0:
-    #         if node.right.bf > 0:
-    #             self.rr(n.right)
-    #             self.rl(n)
-    #     else:
-    #         self.rl(n)
-    #     elif n.bf > 0:
-    #     if n.lf.bf < 0:
-    #         self.rl(node.leftChild)
-    #         self.rr(node)
-    #     else:
-    #         self.rr(node)
+    def lr(self,r_root):
+        """Perform a left rotation (lr) around the rotation_root (r_root)."""
+        #assume:
+        #   a
+        #    \
+        #     b
+        #      \
+        #       c
+        #1. b becomes the new root
+        #2. a takes responsibility of b's left child, in this case None.
+        #3. b makes a it's left child
+        n_root = r_root.right
+        r_root.right = n_root.left
+        if n_root.left != None:
+            n_root.left.parent = r_root
+            n_root.parent = r_root.parent
+        if r_root.is_root():
+            self.root = n_root
+        else:
+            if r_root.is_left():
+                r_root.parent.left = n_root
+            else:
+                r_root.parent.right = n_root
+        n_root.left = r_root
+        r_root.parent = n_root
