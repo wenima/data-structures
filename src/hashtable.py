33c90cd8cba21a00afaa54ebe89f2bf448e258e1
Ford Fowler <fordjfowler@gmail.com>
2017-01-24 14:25:00 -0800

    fnv, set, starting name=main block

--- src/hashtable.py
+++ src/hashtable.py
@@ -15,8 +15,8 @@ class HashTable(object):
             self.hash_func = self._additive_hash
         elif inpt.replace('-', '') == 'oneatatime' or inpt == 'oat':
             self.hash_func = self._oat_hash
-        elif inpt == '':
-            self.hash_func = self._
+        elif inpt == 'fnv':
+            self.hash_func = self._fnv
         else:
             raise NameError("Hash function " + hash_func + " is not an option.")
 
@@ -25,13 +25,29 @@ class HashTable(object):
 
     def set(self, key, val):
         """Store the given val using the given key."""
-        if type(key) is str:
-            idx = self._hash(key) % self.size
-            self.buckets[idx].append(val)
+        bucket = self._get_bucket(key)
+        idx = self._find_idx(key, bucket)
+        if idx is not None:
+            bucket[idx] = (key, val)
+        else:
+            bucket.append((key, val))
+
+    def _get_bucket(self, key):
+        idx = self._hash(key) % self.size
+        return self.buckets[idx]
+
+    def _find_idx(self, key, bucket):
+        for idx, kv_pair in enumerate(bucket):
+            if kv_pair[0] == key:
+                return idx
+        return
 
     def _hash(self, key):
         """Hash the provided key."""
-        return self.hash_func(key)
+        if type(key) is str:
+            return self.hash_func(key)
+        else:
+            raise TypeError('Key must be a string.')
 
     def _oat_hash(self, key):
         h = 0
@@ -45,3 +61,21 @@ class HashTable(object):
         h += h << 15
 
         return h
+
+    def _additive_hash(self, key):
+        h = 0
+        for i in range(len(key)):
+            h += ord(key[i])
+        return h
+
+    def _fnv_hash(self, key):
+        h = 2166136261
+        for i in range(len(key)):
+            h = (h * 16777619) ^ key[i]
+        return h
+
+
+if __name__ == '__main__':
+    ht_add = HashTable()
+    ht_oat = HashTable(hash_func='oat')
+    ht_fnv = HashTable(hash_func='fnv')
