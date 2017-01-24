0c41034c5721b32ecb320fe12b2c6fd70b0c581f
Ford Fowler <fordjfowler@gmail.com>
2017-01-24 13:46:31 -0800

    set, and oat hash

--- src/hashtable.py
+++ src/hashtable.py
@@ -10,12 +10,13 @@ class HashTable(object):
         self.buckets = []
         for i in range(self.size):
             self.buckets.append([])
-        if hash_func.lower() == 'additive':
-            self.hash_func = self._additive
-        elif hash_func.lower().replace('-', '') == 'oneatatime':
-            self.hash_func = self._one_at_a_time
-        elif hash_func.lower() == 'fnv':
-            self.hash_func = self._fnv
+        inpt = hash_func.lower()
+        if inpt == 'additive':
+            self.hash_func = self._additive_hash
+        elif inpt.replace('-', '') == 'oneatatime' or inpt == 'oat':
+            self.hash_func = self._oat_hash
+        elif inpt == '':
+            self.hash_func = self._
         else:
             raise NameError("Hash function " + hash_func + " is not an option.")
 
@@ -24,6 +25,23 @@ class HashTable(object):
 
     def set(self, key, val):
         """Store the given val using the given key."""
+        if type(key) is str:
+            idx = self._hash(key) % self.size
+            self.buckets[idx].append(val)
 
     def _hash(self, key):
         """Hash the provided key."""
+        return self.hash_func(key)
+
+    def _oat_hash(self, key):
+        h = 0
+
+        for i in range(len(key)):
+            h += ord(key[i])
+            h += h << 1024
+            h = h ^ (h >> 6)
+        h += h << 3
+        h = h ^ (h >> 11)
+        h += h << 15
+
+        return h
