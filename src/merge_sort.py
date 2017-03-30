aee4413f069163302a9188a3d08416db856bd8de
MarcMBPDev <marc.wenicker@gmail.com>
2017-03-30 13:32:19 -0700

    working recursive solution

--- src/merge_sort.py
+++ src/merge_sort.py
@@ -1,5 +1,23 @@
 """Module for implementation of merge_sort."""
 
-def merge_sort(l):
+def merge_sort(arr):
     """Return a sorted list using merge_sort algorithm."""
-    pass
+    if len(arr) < 2 :
+        return arr
+    return merge(merge_sort(arr[:len(arr) // 2]), merge_sort(arr[len(arr) // 2:]))
+
+def merge(l, r):
+    """Return an accumulated list after elements have been swapped."""
+    result = []
+    i = 0
+    j = 0
+    while i < len(l) and j < len(r):
+        if l[i] < r[j]:
+            result.append(l[i])
+            i += 1
+        else:
+            result.append(r[j])
+            j += 1
+    result += l[i:]
+    result += r[j:]
+    return result
